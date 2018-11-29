d4e6396f50f4162bef586ff026abd98cca4836a0
Alfat Saputra Harun <harunalfat@gmail.com>
2018-11-29 20:06:21 +0800

    initial commit

---
+++ Lafzi/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lafzi
+//
+//  Created by Alfat Saputra Harun on 28/11/18.
+//  Copyright © 2018 Alfat Saputra Harun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
